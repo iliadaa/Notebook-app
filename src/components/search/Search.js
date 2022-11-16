0f64c1f502ae4596996cd3cc6062934590a14672
iliadaa <iliadaarapi1@gmail.com>
2022-11-16 17:35:29 +0100

    update filter

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Search.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import dummynotes from "../../dummyNotes.json"
-import Notes from "../notes/Notes"
+import dummynotes from "../../dummyNotes.json";
+import Notes from "../notes/Notes";
+//import NotesList from "../noteslist/NotesList";
 
 function Search() {
   // const { items } = props
   //const dummynotestag = dymmynotes
-  const [items, setItems] = useState();
+  //  const [items, setItems] = useState();
   const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    setItems();
-  }, []);
-
-
+  //useEffect(() => {
+  //  setItems();
+  // }, []);
 
   return (
     <>
@@ -22,27 +21,29 @@ function Search() {
         <button className="search-button">
           <SearchOutlinedIcon style={{ fontSize: 26 }} />
         </button>
-        <input value={search}
-          className="search-input" type="text" placeholder="Search..." onChange={(event) => { setSearch(event.target.value) }} />
+        <input
+          value={search}
+          className="search-input"
+          type="text"
+          placeholder="Search..."
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
       </div>
       <div>
         <h3>All Notes</h3>
-
-        {
-          dummynotes.filter((val) => {
-            if (search == "") {
-              return val;
-            } else if (val.toLowerCase().includes(search.toLowerCase())) {
-
-              return val;
-            }
-          },)
-            .map((val, index) => {
-              console.log(val)
-              return (<Notes key={index} value={val} id={val.id} tag={val.tags} />
-              )
+        <div>
+          {dummynotes
+            .filter((notes) => {
+              notes.tags.filter((tag) =>
+                Object.values(tag).includes(search.toLowerCase())
+              );
             })
-        }
+            .map((note) => {
+              return <Notes value={note} tag={note.tags} />;
+            })}
+        </div>
       </div>
     </>
   );
